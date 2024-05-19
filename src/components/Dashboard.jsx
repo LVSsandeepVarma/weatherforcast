d6a76485c7b92d09e85fd1612ea82d08db81a244
Sandeep Varma LVS <sandeepvarmalvs@Sandeeps-Laptop.local>
2024-05-19 22:47:22 +0530

    added https

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -29,7 +29,7 @@ export default function Dashboard() {
 
     const fetchGeolocation = async () => {
         setShowLoader(true)
-        const response = await fetch("http://ip-api.com/json");
+        const response = await fetch("https://ip-api.com/json");
         const data = await response.json();
         console.log(data, "data");
 
